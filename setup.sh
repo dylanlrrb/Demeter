62a83084e9d3450f6421d321c79178288c6fe2e7
Dylan Larrabee <dylanlarrabee6@gmail.com>
2020-01-26 19:59:10 -0800

    create setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install fish -y
+cd .config/fish
+git clone https://github.com/dylanlrrb/fishsticks.git
+cd fishsticks
+os="$(uname -s)"
+if [ $os -eq "Darwin" ]
+then
+    git checkout macos
+else
+    git checkout linux
+fi
+cp config.fish ..
+chsh -s `which fish`
+cd ~
