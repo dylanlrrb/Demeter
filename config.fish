8723126136ff5c2a7d20f674a74434cd9a42f03b
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2020-01-26 21:16:38 -0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -93,11 +93,4 @@ alias restrict "chmod 400 "
 # for opening up permissions on .sh scripts
 alias permit "chmod +x "
 
-### Bash-Git-Prompt working with Fish
-if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]
-then
-  GIT_PROMPT_ONLY_IN_REPO=1
-  source $HOME/.bash-git-prompt/gitprompt.sh
-end
-
 updatefish
