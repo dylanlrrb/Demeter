f4d1d4883e5542b0c52c438eca53e6c9deeb5e6a
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2020-01-26 21:11:57 -0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -141,6 +141,24 @@ nvm use v8.11.1
 ### Bash-Git-Prompt working with Fish
 if [ -f "$HOME/.bash-git-prompt/gitprompt.fish" ]
   set GIT_PROMPT_ONLY_IN_REPO 1
+  set GIT_PROMPT_FETCH_REMOTE_STATUS 0   # uncomment to avoid fetching remote status
+  set GIT_PROMPT_IGNORE_SUBMODULES 1 # uncomment to avoid searching for changed files in submodules
+  set GIT_PROMPT_WITH_VIRTUAL_ENV 0 # uncomment to avoid setting virtual environment infos for node/python/conda environments
+
+  set GIT_PROMPT_SHOW_UPSTREAM 1 # uncomment to show upstream tracking branch
+  set GIT_PROMPT_SHOW_UNTRACKED_FILES normal # can be no, normal or all; determines counting of untracked files
+
+  set GIT_PROMPT_SHOW_CHANGED_FILES_COUNT 0 # uncomment to avoid printing the number of changed files
+
+  # set GIT_PROMPT_STATUS_COMMAND gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
+
+  # set GIT_PROMPT_START ...    # uncomment for custom prompt start sequence
+  # set GIT_PROMPT_END ...      # uncomment for custom prompt end sequence
+
+  # as last entry source the gitprompt script
+  # set GIT_PROMPT_THEME Custom # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
+  # set GIT_PROMPT_THEME_FILE ~/.git-prompt-colors.sh
+  set GIT_PROMPT_THEME Solarized # use theme optimized for solarized color scheme
   source $HOME/.bash-git-prompt/gitprompt.fish
 end
 
