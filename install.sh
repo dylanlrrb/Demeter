ea6c90b6c56a10ad25660116571697be33669708
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2020-08-27 11:27:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,3 +29,8 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
+# steam stuff (if needed)
+# sudo apt-get install nvidia-driver-430
+# there may be more up to date and better versions of nvidia-driver, eg. nvidia-driver-435, nvidia-driver-440 etc.
+# sudo dpkg --configure -a
+# reboot computer
